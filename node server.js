c67617105c505ade2f43480be6200e46d04427ad
Iamneo <techadmin@iamneo.ai>
2023-09-10 11:51:53 +0000

    Backup Commit

--- node server.js
+++ node server.js
@@ -1,1 +1,10 @@
-let board=[",",""]
+let board=[",",",",",",",",",]
+const player1='X';
+const player2='O';
+let currentplayer=player1;
+function handleMove(index){
+    if(board[index]==="){
+        board[index]=currentplayer;
+        document.getElementsByClassName('cell')
+    [i]    }
+}
